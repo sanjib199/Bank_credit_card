5c65d66350c8f8d684ba7ae8feeacb75991bb1e9
Sanjib Basak <107076933+sanjib199@users.noreply.github.com>
2025-10-24 12:25:41 +0530

    update file.

--- due_amount.java
+++ due_amount.java
@@ -1,5 +1,5 @@
 total amount due: - 70 Lakhs
 amount collected: - 45 Lakhs
 due amount: -30 Lakhs
-last date: - 20/12/2025
-please inform to all tem member thank you.
+all due is cleared. no pending right now.
+  thanks to all team member
